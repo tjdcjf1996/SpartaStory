6f025a8ffd27c8d940e70bed1b36e80d2d36fe8e
tjdcjf1996 <r_louis@naver.com>
2024-09-09 22:01:06 +0900

    env 수정

---
+++ src/config.js
@@ -0,0 +1,5 @@
+import { config } from "dotenv";
+config();
+
+export const PORT = process.env.PORT;
+export const SECRET_CODE = process.env.SECRET_CODE;
--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -2,8 +2,8 @@ import express from "express";
 import { prisma } from "../utils/prisma/index.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-// import jwt from "jsonwebtoken";
-// import authMiddleware from "../middlewares/auth.middleware.js";
+import authMiddleware from "../middlewares/auth.middleware.js";
+import { SECRET_CODE } from "../config.js";
 
 const router = express.Router();
 
@@ -32,7 +32,7 @@ router.post("/sign-up", async (req, res, next) => {
       errorMessage: "비밀번호와 확인이 일치하지 않습니다.",
     });
 
-  const hashedPw = await bcrypt.hash(userPw, 7);
+  const hashedPw = await bcrypt.hash(userPw, 10);
   const user = await prisma.users.create({
     data: { userId, userPw: hashedPw, userName },
   });
@@ -58,9 +58,31 @@ router.post("/sign-in", async (req, res, next) => {
     return res.status(400).json({
       errorMessage: "틀린 비밀번호 입니다.",
     });
-  const token = jwt.sign({ userId: isUser.userId }, "sparta");
+  const token = jwt.sign({ userId: isUser.userId }, SECRET_CODE);
   res.setHeader("Authorization", `Bearer ${token}`);
-  return res.status(200).json({ message: "로그인 성공" });
+  return res
+    .status(200)
+    .json({ message: "로그인 성공, 헤더에 토큰값이 반환되었습니다." });
+});
+
+router.post("/c", authMiddleware, async (req, res, next) => {
+  const { characterName } = req.body;
+  const { userId } = req.user;
+  const isCharacter = await prisma.characters.findFirst({
+    where: { characterName },
+  });
+  if (isCharacter)
+    return res.status(400).json({ message: "이미 있는 캐릭터명입니다. " });
+
+  const character = await prisma.characters.create({
+    data: {
+      characterName,
+      userId: +userId,
+    },
+  });
+  return res.status(200).json({
+    data: { character },
+  });
 });
 
 export default router;
