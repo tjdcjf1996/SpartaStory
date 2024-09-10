e08bb609aaf1ce0f1f3080c3d20156b983da5e94
tjdcjf1996 <r_louis@naver.com>
2024-09-10 16:12:51 +0900

    item 라우터 필수기능 구현

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 import express from "express";
 import userRouter from "./routes/users.router.js";
 import characterRouter from "./routes/characters.router.js";
+import itemRouter from "./routes/items.router.js";
 import { PORT } from "./config.js";
 
 const app = express();
 
 app.use(express.json());
-app.use("/api", [userRouter, characterRouter]);
+app.use("/api", [userRouter, characterRouter, itemRouter]);
 
 app.listen(PORT, () => {
   console.log(PORT, "포트로 서버가 열렸어요!");
---
+++ src/routes/items.router.js
@@ -0,0 +1,73 @@
+import express from "express";
+import { prisma } from "../utils/prisma/index.js";
+
+const router = express.Router();
+
+// item 등록
+
+router.post("/item", async (req, res, next) => {
+  const { itemNo, itemName, itemStat, itemPrice } = req.body;
+  const isItem = await prisma.items.findFirst({
+    where: { itemNo },
+  });
+  if (isItem)
+    return res.status(400).json({ message: "이미 있는 아이템 넘버입니다." });
+
+  const item = await prisma.items.create({
+    data: {
+      itemNo,
+      itemName,
+      itemPrice,
+      itemStat,
+    },
+  });
+
+  return res.status(200).json({ data: item });
+});
+
+// item 수정
+
+router.patch("/updateItem/:itemNo", async (req, res, next) => {
+  const { itemNo } = req.params;
+  const { itemName, itemStat } = req.body;
+
+  const Item = await prisma.items.findFirst({
+    where: { itemNo: +itemNo },
+  });
+
+  if (!Item) return res.status(400).json({ message: "없는 아이템입니다." });
+
+  const updateItem = await prisma.items.update({
+    where: { itemNo: +itemNo },
+    data: {
+      itemName,
+      itemStat,
+    },
+  });
+
+  return res.status(200).json({ data: updateItem });
+});
+
+// item 목록 조회
+router.get("/item", async (req, res, next) => {
+  const items = await prisma.items.findMany();
+  if (!items)
+    return res.status(400).json({ message: "등록된 아이템이 없습니다." });
+
+  return res.status(200).json({ data: items });
+});
+
+// item 목록 상세조회
+
+router.get("/item/:itemNo", async (req, res, next) => {
+  const { itemNo } = req.params;
+  const items = await prisma.items.findFirst({
+    where: { itemNo: +itemNo },
+  });
+  if (!items)
+    return res.status(400).json({ message: "등록된 아이템이 없습니다." });
+
+  return res.status(200).json({ data: items });
+});
+
+export default router;
