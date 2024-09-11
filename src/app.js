376c88cedf6e11be7445fb45df14fa0d2ca44372
tjdcjf1996 <r_louis@naver.com>
2024-09-11 15:52:06 +0900

    shopRouter 추가

--- src/app.js
+++ src/app.js
@@ -2,12 +2,13 @@ import express from "express";
 import userRouter from "./routes/users.router.js";
 import characterRouter from "./routes/characters.router.js";
 import itemRouter from "./routes/items.router.js";
+import shopRouter from "./routes/shop.router.js";
 import { PORT } from "./config.js";
 
 const app = express();
 
 app.use(express.json());
-app.use("/api", [userRouter, characterRouter, itemRouter]);
+app.use("/api", [userRouter, characterRouter, itemRouter, shopRouter]);
 
 app.listen(PORT, () => {
   console.log(PORT, "포트로 서버가 열렸어요!");
