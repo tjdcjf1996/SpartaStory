62a5d658992716d546e78fc81c8f9251827426a8
tjdcjf1996 <r_louis@naver.com>
2024-09-11 15:52:21 +0900

    대소문자 수정

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -4,9 +4,9 @@ import { SECRET_CODE } from "../config.js";
 
 export default async function (req, res, next) {
   try {
-    const { Authorization } = req.headers;
+    const { authorization } = req.headers;
 
-    if (!Authorization) throw new Error("요청한 사용자의 토큰이 없습니다.");
+    if (!authorization) throw new Error("요청한 사용자의 토큰이 없습니다.");
 
     const [tokenType, token] = authorization.split(" ");
     if (tokenType !== "Bearer")
